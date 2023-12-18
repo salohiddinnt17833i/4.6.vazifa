508de22089d290f936f73dcff0378427394a7f82
salohiddinnt17833i <salohiddindevoleper@gmail.com>
2023-12-18 20:39:26 +0500

    ozgarish kiritildi

--- index.js
+++ index.js
@@ -11,26 +11,34 @@
 // console.log(res);
 
 //  1 masala
-function oneFunction(num1, num2, num3){
-    let max;
-    if (num1 > num2) {
-        max = num1
-    } else {
-        max = num2
-    }
-    if (max > num3) {
-        max = max
-    } else {
-        max = num3
-    }
-    return max
-}
-console.log(oneFunction(12, 5, 11));
+// function oneFunction(num1, num2, num3){
+//     let max;
+//     if (num1 > num2) {
+//         max = num1
+//     } else {
+//         max = num2
+//     }
+//     if (max > num3) {
+//         max = max
+//     } else {
+//         max = num3
+//     }
+//     return max
+// }
+// console.log(oneFunction(12, 5, 11));
 
 // 2 masala
 
+// // 3 masala
 
-
+// function threeFunction(n = 10){
+//     let counter = 0;
+//     for(let i = 1; i <= n; i += 2){
+//         counter += 1;
+//     }
+//     return counter
+// }
+// threeFunction(10)
 
 
 
